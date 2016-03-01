a9513f2a47f6e42f67ff9c460cca4972e9cc3a59
Metel Patel <metelpatel@gmail.com>
2016-02-29 20:05:45 -0600

    sending users to db

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/signup', function(req, res) {
+  res.render('signup');
+});
+
+router.post('/signup', function(req, res){
+  var name = req.body.name;
+  var email = req.body.email;
+  var password = req.body.password;
+  var businessName = req.body.businessName;
+  var surveyName = req.body.surveyName;
+
+  var newUser = User({
+      name: name,
+      email: email,
+      password: password,
+      businessName: businessName,
+      surveyName: surveyName
+  });
+    newUser.save(function(err) {
+        if (err) console.log(err);
+    });
+  res.redirect('/');
+})
 
 module.exports = router;
